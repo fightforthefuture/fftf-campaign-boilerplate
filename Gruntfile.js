8b934ce89da7ee946709cc60d29bf2fc124d797b
Jeff Lyon <jeff@rubbingalcoholic.com>
2016-03-23 14:04:42 -0500

    update boilerplate with GPL v3 licensing

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,8 @@ module.exports = function (grunt) {
   require('time-grunt')(grunt);
   require('jit-grunt')(grunt, {});
 
+  var saveLicense = require('uglify-save-license');
+
   grunt.initConfig({
     site: {
       app: 'app',
@@ -143,13 +145,13 @@ module.exports = function (grunt) {
     concat: {
       options: {
         sourceMap: true,
-        separator: grunt.util.linefeed + ';',
-        banner: '/*\n @licstart  The following is the entire license notice for the\n    JavaScript code in this page.\n\n Copyright (C) 2015 Fight for the Future\n\n The JavaScript code in this page is free software: you can\n redistribute it and/or modify it under the terms of the GNU\n General Public License (GNU GPL) as published by the Free Software\n Foundation, either version 3 of the License, or (at your option)\n any later version. The code is distributed WITHOUT ANY WARRANTY;\n without even the implied warranty of MERCHANTABILITY or FITNESS\n FOR A PARTICULAR PURPOSE. See the GNU GPL for more details.\n\n As additional permission under GNU GPL version 3 section 7, you\n may distribute non-source (e.g., minimized or compacted) forms of\n that code without the copy of the GNU GPL normally required by\n section 4, provided you include this license notice and a URL\n through which recipients can access the Corresponding Source.\n\n @licend  The above is the entire license notice\n    for the JavaScript code in this page.\n*/\n'
+        separator: grunt.util.linefeed + ';'
       },
       server: {
         files: [
           {
             src: [
+              '<%= site.app %>/_js/LICENSE',
               '<%= site.app %>/_js/controllers/**/*.js',
               '<%= site.app %>/_js/models/**/*.js',
               '<%= site.app %>/_js/views/**/*.js',
@@ -166,7 +168,7 @@ module.exports = function (grunt) {
         sourceMap: true,
         sourceMapIncludeSources: true,
         check: 'gzip',
-        banner: '/*\n @licstart  The following is the entire license notice for the\n    JavaScript code in this page.\n\n Copyright (C) 2015 Fight for the Future\n\n The JavaScript code in this page is free software: you can\n redistribute it and/or modify it under the terms of the GNU\n General Public License (GNU GPL) as published by the Free Software\n Foundation, either version 3 of the License, or (at your option)\n any later version. The code is distributed WITHOUT ANY WARRANTY;\n without even the implied warranty of MERCHANTABILITY or FITNESS\n FOR A PARTICULAR PURPOSE. See the GNU GPL for more details.\n\n As additional permission under GNU GPL version 3 section 7, you\n may distribute non-source (e.g., minimized or compacted) forms of\n that code without the copy of the GNU GPL normally required by\n section 4, provided you include this license notice and a URL\n through which recipients can access the Corresponding Source.\n\n @licend  The above is the entire license notice\n    for the JavaScript code in this page.\n*/\n'
+        preserveComments: saveLicense
       },
       build: {
         files: {
